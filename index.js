41608e54b2cbdb0c86a788af7451eee3e204fc13
Travis <tjwokasch@gmail.com>
2021-08-11 09:27:58 -0600

    commit tree 6705fd9d59130071fcb0164ab5ad5a28316b8d11

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Your code here
+class Polygon {
+    constructor(integers){
+    this.integers = integers
+    }
+
+    get countSides() {
+       return this.integers.length
+    }
+
+    get perimeter() {
+        let sum = (this.integers).reduce(function(a, b) {return a + b}, 0)
+        return sum
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        if ((this.integers[0]+this.integers[1]) <= this.integers[2] || (this.integers[1]+this.integers[2]) <= this.integers[0] || (this.integers[0]+this.integers[2]) <= this.integers[1]){
+        return false 
+            }
+        else {
+            return true
+            }
+    }
+}
+
+class Square extends Polygon {
+    get isValid(){
+        if (this.integers[0] === this.integers[1]) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    get area() {
+        let area = this.integers[0] * this.integers[1]
+        return area
+    }
+}
+
+// polygon = new Polygon ([5, 5, 5])
+// console.log(polygon.perimeter)
